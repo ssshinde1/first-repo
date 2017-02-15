9e367cb3775147763f7e00cbed1915b66ad3b502
BA201 <BA201@MCBA201-C06.classrooms.stlcc>
2017-02-15 13:26:59 -0600

    Add another line in Main class

--- src/Console.java
+++ src/Console.java
@@ -11,6 +11,7 @@
 public class Console {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("Yey, I created a report on GitHub");
         
     }
           
