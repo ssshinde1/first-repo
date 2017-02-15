b0e788cf19a77518fa5d7dcb21d0699a9f0c4363
BA201 <BA201@MCBA201-C06.classrooms.stlcc>
2017-02-15 13:07:47 -0600

    first commit

---
+++ src/Console.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BA201
+ */
+public class Console {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        
+    }
+          
+}
